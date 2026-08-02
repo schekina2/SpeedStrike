1f2b0191199db1ea2610566c0d18ffb588d77554
Schekina <schekina.ahounou@epitech.eu>
2026-08-02 18:09:01 +0100

    Drew spiky enemy shape instead of plain square

--- scripts/Enemies.gd
+++ scripts/Enemies.gd
@@ -54,7 +54,44 @@ func spawn_shatter_effect(grid_pos: Vector2i):
 func _draw():
 	for pos in enemies:
 		var draw_pos = pos * Constants.GRID_SIZE
-		draw_rect(Rect2(draw_pos.x, draw_pos.y, Constants.GRID_SIZE - 2, Constants.GRID_SIZE - 2), Color(0.8, 0.1, 0.6))
+		draw_enemy_body(draw_pos)
+		draw_enemy_eyes(draw_pos)
+
+func draw_enemy_body(draw_pos: Vector2):
+	var center = draw_pos + Vector2(Constants.GRID_SIZE / 2.0, Constants.GRID_SIZE / 2.0)
+	var radius = Constants.GRID_SIZE / 2.0 - 2
+	var spike_count = 8
+	var points = PackedVector2Array()
+
+	for i in range(spike_count * 2):
+		var angle = (TAU / (spike_count * 2)) * i
+		var r = radius if i % 2 == 0 else radius * 0.65
+		points.append(center + Vector2(cos(angle), sin(angle)) * r)
+
+	draw_polygon(points, PackedColorArray([Color(0.75, 0.1, 0.55)]))
+
+	for i in range(points.size()):
+		var next_point = points[(i + 1) % points.size()]
+		draw_line(points[i], next_point, Color(0.4, 0.05, 0.3), 1.5)
+
+func draw_enemy_eyes(draw_pos: Vector2):
+	var eye_radius = 3.0
+	var offset = Constants.GRID_SIZE / 4.0
+
+	var eye1_pos = Vector2(draw_pos.x + offset, draw_pos.y + offset)
+	var eye2_pos = Vector2(draw_pos.x + Constants.GRID_SIZE - offset, draw_pos.y + offset)
+
+	draw_circle(eye1_pos, eye_radius, Color.WHITE)
+	draw_circle(eye2_pos, eye_radius, Color.WHITE)
+	draw_circle(eye1_pos, eye_radius * 0.5, Color.BLACK)
+	draw_circle(eye2_pos, eye_radius * 0.5, Color.BLACK)
+
+	var mouth_y = draw_pos.y + Constants.GRID_SIZE - offset
+	draw_line(
+		Vector2(draw_pos.x + offset, mouth_y),
+		Vector2(draw_pos.x + Constants.GRID_SIZE - offset, mouth_y),
+		Color.BLACK, 2.0
+	)
 
 func set_speed(new_speed: float):
 	enemy_speed = new_speed
